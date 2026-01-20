d32f453dc7a0af85fc7d051970f08040cfbdaf99
olzhix123-alt <olzhix123@gmail.com>
2026-01-20 11:10:39 +0500

    schema.sql

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,4 +1,4 @@
--- Тағамдар мен сусындар кестесі
+
 CREATE TABLE menu_items (
                             id SERIAL PRIMARY KEY,
                             name VARCHAR(100) NOT NULL,
@@ -6,13 +6,13 @@ CREATE TABLE menu_items (
                             type VARCHAR(50) NOT NULL -- 'Food' немесе 'Drink' екенін анықтау үшін
 );
 
--- Тапсырыстар кестесі (Foreign Key қолдану үшін) [cite: 63, 65]
+
 CREATE TABLE orders (
                         id SERIAL PRIMARY KEY,
                         item_id INT REFERENCES menu_items(id), -- Бұл Foreign Key
                         order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Тесттік деректер (Sample Inserts) [cite: 67]
+
 INSERT INTO menu_items (name, price, type) VALUES ('Pizza Margherita', 2500, 'Food');
 INSERT INTO menu_items (name, price, type) VALUES ('Coca Cola', 500, 'Drink');

83cbdfb63df931eae6fc9139f376924bf9804c77
olzhix123-alt <olzhix123@gmail.com>
2026-01-20 11:11:13 +0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE menu_items (
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(100) NOT NULL,
+                            price DECIMAL(10, 2) NOT NULL,
+                            type VARCHAR(50) NOT NULL -- 'Food' немесе 'Drink' екенін анықтау үшін
+);
+
+
+CREATE TABLE orders (
+                        id SERIAL PRIMARY KEY,
+                        item_id INT REFERENCES menu_items(id), -- Бұл Foreign Key
+                        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO menu_items (name, price, type) VALUES ('Pizza Margherita', 2500, 'Food');
+INSERT INTO menu_items (name, price, type) VALUES ('Coca Cola', 500, 'Drink');
